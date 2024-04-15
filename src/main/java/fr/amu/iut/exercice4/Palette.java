af78a5118720df1271ad7ac35bde68ffabadb02d
GRAVOILLE-Mathieu-2326055aa <mathieu.gravoille@etu.univ-amu.fr>
2024-04-15 11:12:47 +0200

    exercice 4

--- src/main/java/fr/amu/iut/exercice4/Palette.java
+++ src/main/java/fr/amu/iut/exercice4/Palette.java
@@ -1,11 +1,12 @@
 package fr.amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Palette extends Application {
@@ -25,7 +26,55 @@ public class Palette extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.root = new BorderPane();
+        this.nbRouge =nbRouge;
+        this.label=new Label();
+        this.nbBleu=nbBleu;
+        this.nbVert=nbVert;
+        this.panneau=new Pane();
+        this.bas=new HBox();
+        this.bleu=new Button("Bleu");
+        this.rouge=new Button("Rouge");
+        this.vert=new Button("Vert");
+
+        Label sp=new Label("  ");
+        Label sp1=new Label("  ");
+        HBox top = new HBox(label);
+        VBox vb = new VBox();
+        top.setAlignment(Pos.CENTER);
+
+        root.setTop(top);
+        root.setCenter(panneau);
+        Label spv = new Label();
+        Label spr=new Label();
+        this.bas = new HBox(vert, sp, rouge, sp1, bleu);
+        vb.getChildren().addAll(spr, bas, spv);
+        bas.setAlignment(Pos.CENTER);
+        root.setBottom(vb);
+        bas.setAlignment(Pos.CENTER);
+
+        vert.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            this.nbVert=nbVert+1;
+            panneau.setStyle("-fx-background-color: green");
+            label.setText("Vert choisi "+nbVert+" fois");
+        });
+
+        bleu.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            this.nbBleu=nbBleu+1;
+            panneau.setStyle("-fx-background-color: blue");
+            label.setText("Bleu choisi "+nbBleu+" fois");
+        });
+
+        rouge.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            this.nbRouge=nbRouge+1;
+            panneau.setStyle("-fx-background-color: red");
+            label.setText("Rouge choisi "+nbRouge+" fois");
+        });
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setHeight(200);
+        primaryStage.setWidth(400);
         primaryStage.show();
     }
 }
-
