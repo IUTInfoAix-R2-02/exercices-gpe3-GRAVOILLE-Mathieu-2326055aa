900aff418813392294a6779f04d6d2aa316bf0c5
mathieu <mathieu.gravoille@etu.univ-amu.fr>
2024-05-20 17:50:38 +0200

    exercice 2 et 7, début

--- src/main/java/fr/amu/iut/exercice2/TicTacToe.java
+++ src/main/java/fr/amu/iut/exercice2/TicTacToe.java
@@ -1,12 +1,70 @@
 package fr.amu.iut.exercice2;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.stage.StageStyle;
 
 public class TicTacToe extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        GridPane grid = new GridPane();
+        Scene scene = new Scene(grid);
+
+        Label label = new Label();
+        Label label2 = new Label();
+        Label label3 = new Label();
+        Label label4 = new Label();
+        Label label5 = new Label();
+        Label label6 = new Label();
+        Label label7 = new Label();
+        Label label8 = new Label();
+        Label label9 = new Label();
+
+        ImageView croix = new ImageView("exercice2/Croix.png");
+        ImageView rond = new ImageView("exercice2/Rond.png");
+        ImageView vide = new ImageView("exercice2/Vide.png");
+
+        label.setGraphic(croix);
+        label2.setGraphic(rond);
+        label3.setGraphic(rond);
+        label4.setGraphic(rond);
+        label5.setGraphic(rond);
+        label6.setGraphic(rond);
+        label7.setGraphic(vide);
+        label8.setGraphic(vide);
+        label9.setGraphic(croix);
+
+        grid.getChildren().addAll(label, label2, label3, label4, label5, label6, label7, label8, label9);
+        GridPane.setConstraints(label, 0, 0);
+        label.setAlignment(Pos.CENTER);
+        GridPane.setConstraints(label2, 1, 0);
+        label2.setAlignment(Pos.CENTER);
+        GridPane.setConstraints(label3, 2, 0);
+        label3.setAlignment(Pos.CENTER);
+        GridPane.setConstraints(label4, 0, 1);
+        label4.setAlignment(Pos.CENTER);
+        GridPane.setConstraints(label5, 1, 1);
+        label5.setAlignment(Pos.CENTER);
+        GridPane.setConstraints(label6, 2, 1);
+        label6.setAlignment(Pos.CENTER);
+        GridPane.setConstraints(label7, 0, 2);
+        label7.setAlignment(Pos.CENTER);
+        GridPane.setConstraints(label8, 1, 2);
+        label8.setAlignment(Pos.CENTER);
+        GridPane.setConstraints(label9, 2, 2);
+        label9.setAlignment(Pos.CENTER);
+
+        primaryStage.setTitle("Tic Tac Toe");
+//        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
--- src/main/java/fr/amu/iut/exercice7/CounterController.java
+++ src/main/java/fr/amu/iut/exercice7/CounterController.java
@@ -1,7 +1,13 @@
 package fr.amu.iut.exercice7;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -9,10 +15,23 @@ public class CounterController implements Initializable {
 
     int counter = 0;
 
+    @FXML
+    BorderPane counterPane;
+    @FXML
+    HBox counterBox;
+    @FXML
+    VBox counterVBox;
+    @FXML
+    Label counterLabel = new Label("0");
+
     public void increment() {
+        counter++;
+        counterLabel = new Label(String.valueOf(counter));
     }
 
     public void decrement() {
+        counter--;
+        counterLabel = new Label(String.valueOf(counter));
     }
 
     @Override
